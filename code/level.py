707f5e033d486a5dd680468799b89c045ead3528
Luca <luca.haar@icloud.com>
2023-06-07 04:35:12 +1000

    fixed border -- fixed the border by copying from source code and slowly adjusting code (genuinely convinced it had to do with blank lines and indentation...thanks python)

--- code/level.py
+++ code/level.py
@@ -1,97 +1,92 @@
-import pygame
-from random import choice
+import pygame 
 from settings import *
-from support import import_csv_layout, import_folder
 from tile import Tile
 from player import Player
 from debug import debug
+from support import *
+from random import choice
 
 class Level:
-    def __init__(self):
+	def __init__(self):
 
-        # get the display surface
-        self.display_surface = pygame.display.get_surface()
-        
-        # sprite group setup
-        self.visible_sprites = YSortCameraGroup()
-        self.obstacle_sprites = pygame.sprite.Group()
+		# get the display surface 
+		self.display_surface = pygame.display.get_surface()
 
-        # sprite setup
-        self.create_map()
+		# sprite group setup
+		self.visible_sprites = YSortCameraGroup()
+		self.obstacle_sprites = pygame.sprite.Group()
 
+		# sprite setup
+		self.create_map()
 
-    def create_map(self):
+	def create_map(self):
         # map dictionary
-        layouts = {
-            "boundary": import_csv_layout("./map/map_FloorBlocks.csv"),
-            "grass": import_csv_layout("./map/map_Grass.csv"),
-            "object": import_csv_layout("./map/map_Objects.csv"),
-        }
-        graphics ={
-            "grass": import_folder("./graphics/Grass"),
-            "objects": import_folder("./graphics/objects"),
-        }
+		layouts = {
+			"boundary": import_csv_layout("./map/map_FloorBlocks.csv"),
+			"grass": import_csv_layout("./map/map_Grass.csv"),
+			"object": import_csv_layout("./map/map_Objects.csv"),
+		}
+		graphics = {
+			"grass": import_folder("./graphics/Grass"),
+			"objects": import_folder("./graphics/objects"),
+		}
 
-        for style, layout in layouts.items():
-            for row_index, row in enumerate(layout):
-                for col_index, col in enumerate(row):
-                    if col != "-1":
-                        x = col_index * TILESIZE
-                        y = row_index * TILESIZE
+		for style, layout in layouts.items():
+			for row_index, row in enumerate(layout):
+				for col_index, col in enumerate(row):
+					if col != "-1":
+						x = col_index * TILESIZE
+						y = row_index * TILESIZE
                         
                         # boundry border
-                        if style == "boundry":
-                            Tile((x, y), [self.visible_sprites, self.obstacle_sprites], "invisible")
-                        
+						if style == "boundary":
+							Tile((x, y), [self.obstacle_sprites], "invisible")
+							
                         # grass tile
-                        if style == "grass":
-                            random_grass_image = choice(graphics["grass"])
-                            Tile((x, y), [self.visible_sprites, self.obstacle_sprites], "grass", random_grass_image)
-                        
+						if style == "grass":
+							random_grass_image = choice(graphics["grass"])
+							Tile((x, y), [self.visible_sprites, self.obstacle_sprites], "grass", random_grass_image)
+
                         # object tile
-                        if style == "object":
-                            surface = graphics["objects"][int(col)]
-                            Tile((x, y), [self.visible_sprites, self.obstacle_sprites], "object", surface)
+						if style == "object":
+							surface = graphics["objects"][int(col)]
+							Tile((x, y), [self.visible_sprites, self.obstacle_sprites], "object", surface)
 
-        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites)
+		self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites)
+		
 
-
-    def run(self):
-        # update and draw the game
-        self.visible_sprites.custom_draw(self.player)
-        self.visible_sprites.update()
-        # debug(self.player.direction)
-
-
+	def run(self):
+		# update and draw the game
+		self.visible_sprites.custom_draw(self.player)
+		self.visible_sprites.update()
 
 
 class YSortCameraGroup(pygame.sprite.Group):
-    def __init__(self):
+	def __init__(self):
 
-        # general setup
-        super().__init__()
-        self.display_surface = pygame.display.get_surface()
+		# general setup 
+		super().__init__()
+		self.display_surface = pygame.display.get_surface()
         # get center of screen
-        self.half_width = self.display_surface.get_size()[0] // 2
-        self.half_height = self.display_surface.get_size()[1] // 2
-        self.offset = pygame.math.Vector2()
+		self.half_width = self.display_surface.get_size()[0] // 2
+		self.half_height = self.display_surface.get_size()[1] // 2
+		self.offset = pygame.math.Vector2()
 
-        #creating the map floor
-        self.floor_surface = pygame.image.load("./graphics/tilemap/ground.png").convert()
-        self.floor_rect = self.floor_surface.get_rect(topleft = (0, 0))
+		# creating the map floor
+		self.floor_surface = pygame.image.load("./graphics/tilemap/ground.png").convert()
+		self.floor_rect = self.floor_surface.get_rect(topleft = (0, 0))
 
+	def custom_draw(self, player):
 
-    def custom_draw(self, player):
-       
-        # getting the offset
-        self.offset.x = player.rect.centerx - self.half_width
-        self.offset.y = player.rect.centery - self.half_height
+		# getting the offset 
+		self.offset.x = player.rect.centerx - self.half_width
+		self.offset.y = player.rect.centery - self.half_height
 
-        # drawing the floor
-        floor_offset_pos = self.floor_rect.topleft - self.offset
-        self.display_surface.blit(self.floor_surface, floor_offset_pos)
+		# drawing the floor
+		floor_offset_pos = self.floor_rect.topleft - self.offset
+		self.display_surface.blit(self.floor_surface, floor_offset_pos)
 
-        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
+		for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
             # anchor camera to player in the center of the screen
-            offset_pos = sprite.rect.topleft - self.offset
-            self.display_surface.blit(sprite.image, offset_pos)
+			offset_pos = sprite.rect.topleft - self.offset
+			self.display_surface.blit(sprite.image, offset_pos)
