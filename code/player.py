44938e7adf1a0b9631eb55c6a1b57378405dd3fe
Luca <luca.haar@icloud.com>
2023-06-07 08:55:37 +1000

    player animations -- attempted to add player animations to the player actions and movement but something broke...will fix later

--- code/player.py
+++ code/player.py
@@ -13,6 +13,8 @@ class Player(pygame.sprite.Sprite):
         # graphics setup
         self.import_player_assets()
         self.status = "down"
+        self.frame_index = 0
+        self.animation_speed = 0.15
 
         # movement
         self.direction = pygame.math.Vector2()
@@ -48,41 +50,43 @@ class Player(pygame.sprite.Sprite):
 
 
     def input(self):
-        keys = pygame.key.get_pressed()
-        mouse_pressed = pygame.mouse.get_pressed()
+        # stop movement while attacking
+        if not self.attacking:
+            keys = pygame.key.get_pressed()
+            mouse_pressed = pygame.mouse.get_pressed()
 
-        # movement input
-        if keys[pygame.K_w]:
-            self.direction.y = -1
-            self.status = "up"
-        elif keys[pygame.K_s]:
-            self.direction.y = 1
-            self.status = "down"
-        else:
-            self.direction.y = 0
+            # movement input
+            if keys[pygame.K_w]:
+                self.direction.y = -1
+                self.status = "up"
+            elif keys[pygame.K_s]:
+                self.direction.y = 1
+                self.status = "down"
+            else:
+                self.direction.y = 0
 
-        if keys[pygame.K_d]:
-            self.direction.x = 1
-            self.status = "right"
-        elif keys[pygame.K_a]:
-            self.direction.x = -1
-            self.status = "left"
-        else:
-            self.direction.x = 0
+            if keys[pygame.K_d]:
+                self.direction.x = 1
+                self.status = "right"
+            elif keys[pygame.K_a]:
+                self.direction.x = -1
+                self.status = "left"
+            else:
+                self.direction.x = 0
 
-        # attack input
-        if mouse_pressed[0] and not self.attacking:
-            # left click
-            self.attacking = True
-            self.attack_time = pygame.time.get_ticks()
-            print("left click - attack")
+            # attack input
+            if mouse_pressed[0]:
+                # left click
+                self.attacking = True
+                self.attack_time = pygame.time.get_ticks()
+                print("left click - attack")
 
-        # magic input
-        if mouse_pressed[2] and not self.attacking:
-            # right click
-            self.attacking = True
-            self.attack_time = pygame.time.get_ticks()
-            print("right click - magic")
+            # magic input
+            if mouse_pressed[2]:
+                # right click
+                self.attacking = True
+                self.attack_time = pygame.time.get_ticks()
+                print("right click - magic")
 
 
     def get_status(self):
@@ -159,8 +163,22 @@ class Player(pygame.sprite.Sprite):
                 self.attacking = False
 
 
+    def animate(self):
+        animation = self.animations[self.status]
+
+        # loop pver the frame index
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+
+        # set the image
+        self.image = animation[int(self.frame_index)]
+        self.rect = self.image.get_rect(center = self.hitbox.center)
+
+
     def update(self):
         self.input()
         self.cooldowns()
         self.get_status()
+        self.animate()
         self.move(self.speed)
